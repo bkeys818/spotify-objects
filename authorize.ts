a560f2d9f9c2598b94950e4312ec68cecc3fe0e8
Ben Keys <bkeys818@gmail.com>
2021-10-05 17:30:19 -0400

    🔨 Update authorize script
    When a value was updated, it would be written to ".env" but not changed in `process.env`.

--- authorize.ts
+++ authorize.ts
@@ -4,123 +4,113 @@ import fetch from 'node-fetch'
 import open from 'open'
 import { readFileSync, writeFileSync } from 'fs'
 
+const envPath = './.env'
+const env = dotenv.parse(readFileSync(envPath))
+const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, EXPIRES_AT } = env
+const scopes = [
+    'ugc-image-upload',
+    'user-read-recently-played',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'app-remote-control',
+    'streaming',
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'user-follow-modify',
+    'user-follow-read',
+    'user-library-modify',
+    'user-library-read',
+    'user-read-email',
+    'user-read-private',
+]
+
 export default async function authorize() {
-    dotenv.config()
     // make sure client_id and client_state are defined
-    if (!('CLIENT_ID' in process.env && process.env.CLIENT_ID)) {
+    if (!CLIENT_ID) {
         throw new Error(
             'Authentication Error! The .env file is missing a CLIENT_ID'
         )
-    } else if (!('CLIENT_SECRET' in process.env && process.env.CLIENT_SECRET)) {
+    } else if (!CLIENT_SECRET) {
         throw new Error(
             'Authentication Error! The .env file is missing a CLIENT_SECRET'
         )
     }
 
     // 1. client_id and client_secret only
-    if (!('REFRESH_TOKEN' in process.env)) {
+    if (!REFRESH_TOKEN) {
         const refreshToken = await getRefreshToken()
         saveToEnv(refreshToken)
-        return
     }
 
     // 2. has refresh_token but token is expired
-    else if (
-        !('EXPIRES_AT' in process.env) ||
-        parseInt(process.env.EXPIRES_AT!) < new Date().valueOf()
-    ) {
+    else if (!EXPIRES_AT || parseInt(EXPIRES_AT) < new Date().valueOf()) {
         const token = await getNewToken()
         saveToEnv(token)
-        return
     }
 
     // 3. token is valid
-    return
+    dotenv.config()
 }
 
-function getRefreshToken(): Promise<RefreshToken> {
-    return new Promise((resolve) => {
-        const host = 'localhost'
-        const port = 8080
-        const redirectUri = `http://${host}:${port}/callback`
+function getRefreshToken() {
+    const host = 'localhost'
+    const port = 8080
+    const redirectUri = `http://${host}:${port}/callback`
 
+    return new Promise((resolve: (value: RefreshToken) => void) => {
         const requestListener: RequestListener = function (req, res) {
             const [path, query] = (req.url ?? '').split('?')
 
-            switch (path) {
-                case '/callback':
-                    if (query && query == '') {
-                        console.error(
-                            'Authorization Error! Redirected to "%s" without authoizing',
-                            req.url
-                        )
-                        process.exit(1)
-                    }
-
-                    const queryObj = parseQuery(query)
-
-                    if ('error' in queryObj) {
-                        console.error(`Authoization Error! ${queryObj.error}`)
-                        process.exit(1)
-                    }
-
-                    if (!('code' in queryObj)) {
-                        console.error(
-                            "Authoization Error! Couldn't find code for authorization\nquery: O",
-                            queryObj
-                        )
-                        process.exit(1)
-                    }
-
-                    postRequest({
-                        grant_type: 'authorization_code',
-                        code: queryObj.code,
-                        redirect_uri: redirectUri,
-                    }).then(value => resolve(value as RefreshToken))
-
-                    res.writeHead(200).end('<script>window.close()</script>')
-                    server.close()
-
-                    break
-
-                default:
-                    const url = new URL(
-                        'https://accounts.spotify.com/authorize'
+            // callback page
+            if (path == '/callback') {
+                if (query && query == '') {
+                    console.error(
+                        'Authorization Error! Redirected to "%s" without authoizing',
+                        req.url
                     )
-                    url.searchParams.set('client_id', process.env.CLIENT_ID!)
-                    url.searchParams.set('response_type', 'code')
-                    url.searchParams.set('redirect_uri', redirectUri)
-                    url.searchParams.set(
-                        'scope',
-                        [
-                            'ugc-image-upload',
-                            'user-read-recently-played',
-                            'user-top-read',
-                            'user-read-playback-position',
-                            'user-read-playback-state',
-                            'user-modify-playback-state',
-                            'user-read-currently-playing',
-                            'app-remote-control',
-                            'streaming',
-                            'playlist-modify-public',
-                            'playlist-modify-private',
-                            'playlist-read-private',
-                            'playlist-read-collaborative',
-                            'user-follow-modify',
-                            'user-follow-read',
-                            'user-library-modify',
-                            'user-library-read',
-                            'user-read-email',
-                            'user-read-private',
-                        ].join()
+                    process.exit(1)
+                }
+
+                const queryObj = parseQuery(query)
+
+                if ('error' in queryObj) {
+                    console.error(`Authoization Error! ${queryObj.error}`)
+                    process.exit(1)
+                }
+
+                if (!('code' in queryObj)) {
+                    console.error(
+                        "Authoization Error! Couldn't find code for authorization\nquery: O",
+                        queryObj
                     )
+                    process.exit(1)
+                }
 
-                    res.writeHead(302, {
-                        Location: url.href,
-                    })
+                postRequest({
+                    grant_type: 'authorization_code',
+                    code: queryObj.code,
+                    redirect_uri: redirectUri,
+                }).then((value) => resolve(value))
 
-                    res.end()
-                    break
+                res.writeHead(200).end('<script>window.close()</script>')
+                server.close()
+            }
+
+            // main page
+            else {
+                const url = new URL('https://accounts.spotify.com/authorize')
+                url.searchParams.set('client_id', CLIENT_ID!)
+                url.searchParams.set('response_type', 'code')
+                url.searchParams.set('redirect_uri', redirectUri)
+                url.searchParams.set('scope', scopes.join())
+
+                res.writeHead(302, { Location: url.href })
+                res.end()
             }
         }
 
@@ -134,20 +124,26 @@ function getRefreshToken(): Promise<RefreshToken> {
     })
 }
 
-async function getNewToken() {
-    return await postRequest({
+function getNewToken() {
+    return postRequest({
         grant_type: 'refresh_token',
-        refresh_token: process.env.REFRESH_TOKEN!,
-    }) as Token
+        refresh_token: REFRESH_TOKEN!,
+    })
 }
 
-async function postRequest(body: { [key: string]: string }) {
+async function postRequest<
+    T extends 'refresh_token' | 'authorization_code'
+>(body: {
+    grant_type: T
+    [key: string]: string
+}): Promise<T extends 'refresh_token' ? Token : RefreshToken> {
+    const auth =
+        'Basic ' +
+        Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')
     const response = await fetch('https://accounts.spotify.com/api/token', {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-            Authorization: `Basic ${Buffer.from(
-                `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
-            ).toString('base64')}`,
+            Authorization: auth,
         },
         method: 'POST',
         body: Object.entries(body)
@@ -155,11 +151,10 @@ async function postRequest(body: { [key: string]: string }) {
             .join('&'),
     })
     if (response.ok) {
-        return await response.json() as { [key: string]: any }
+        return await response.json()
     } else {
         let msg
         try {
-            // @ts-ignore
             msg = (await response.json()).error
         } catch {
             msg = 'Unable to send POST request.'
@@ -171,12 +166,10 @@ async function postRequest(body: { [key: string]: string }) {
 
 function saveToEnv(value: Token | RefreshToken) {
     delete value.scope
-
     const expiresAt = new Date().valueOf() + value.expires_in * 1000
     // @ts-ignore
     delete value.expires_in
 
-    const envPath = './.env'
     try {
         const obj = dotenv.parse(readFileSync(envPath))
         delete obj.ACCESS_TOKEN
@@ -195,8 +188,6 @@ function saveToEnv(value: Token | RefreshToken) {
         console.error(`Authorization Error! ${error}`)
         process.exit(1)
     }
-
-    return
 }
 
 function parseQuery(query: string) {
