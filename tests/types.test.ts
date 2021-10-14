d6f6949d283dad7ba7717b015951f4eee5f84d77
Ben Keys <bkeys818@gmail.com>
2021-10-14 15:43:09 -0400

    🧪 Add test for types

---
+++ tests/types.test.ts
@@ -0,0 +1,14 @@
+import responses from '../responses'
+import { ajv, idFor } from './schemas'
+
+const responseNames = Object.keys(responses) as (keyof typeof responses)[]
+test.concurrent.each(responseNames)('Schema for %s responses', (responseName) => {
+    const response = responses[responseName]
+    expect(response).toEqual(expect.any(Object))
+
+    const id = idFor(responseName)
+    const validate = ajv.getSchema<typeof response>(id)!
+    expect(validate).toBeDefined()
+
+    expect(response).toPass(validate)
+})
