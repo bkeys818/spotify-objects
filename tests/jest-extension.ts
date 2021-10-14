1e7fcb3dbb78ec642f170100f3516b8843a0ebc9
Ben Keys <bkeys818@gmail.com>
2021-10-14 16:53:23 -0400

    ⚗️ Add instance to error object

--- tests/jest-extension.ts
+++ tests/jest-extension.ts
@@ -1,5 +1,11 @@
-import type { ValidateFunction, ErrorObject } from 'ajv'
+import type { ValidateFunction } from 'ajv'
 import { prettyPrint } from '@base2/pretty-print-object'
+
+const prettyPrintConfig: Parameters<typeof prettyPrint>[1] = {
+    indent: '  ',
+    inlineCharacterLimit: 64,
+    singleQuotes: true,
+}
 
 function toPass<T>(
     this: jest.MatcherContext,
@@ -12,18 +18,49 @@ function toPass<T>(
         promise: this.promise,
     })
     msg += '\n\n'
-    if (validation.errors)
-        msg +=
-            `Errors: ${prettyPrint(validation.errors, {
-                indent: '  ',
-                inlineCharacterLimit: 64,
-                singleQuotes: true,
-            })}`
-    else msg += 'Object passed validation.'
+    if (validation.errors) {
+        const errors = validation.errors.map((error) => {
+            const newError: typeof error & { instance?: any } = error
+            try {
+                const instance = getInstance(received, error.instancePath)
+                newError.instance = foldObject(instance)
+            } catch {}
+            return newError
+        })
+        msg += 'Errors: ' + format(prettyPrint(errors, prettyPrintConfig))
+    } else msg += 'Object passed validation.'
     return {
         pass: pass,
         message: () => msg,
     }
+}
+
+function getInstance(_obj: any, path: string) {
+    if (!path) return _obj
+    let obj = JSON.parse(JSON.stringify(_obj))
+    if (path.startsWith('/')) path = path.slice(1)
+    const steps = path.split('/')
+    for (const step of steps) {
+        if (step in obj) obj = obj[step]
+        else throw new Error(`Couldn't get instance`)
+    }
+    return obj
+}
+
+function foldObject(value: any) {
+    if (value && typeof value == 'object')
+        for (const key in value) {
+            if (Array.isArray(value[key])) value[key] = '___arr___'
+            else if (value[key] && typeof value[key] == 'object')
+                value[key] = '___obj___'
+        }
+    return value
+}
+
+function format(value: string) {
+    return value
+        .replace(/'___arr___'/g, '[ ... ]')
+        .replace(/'___obj___'/g, '{ ... }')
 }
 
 expect.extend({ toPass })
